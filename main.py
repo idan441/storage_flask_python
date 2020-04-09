51436b1f252ca8a310901665be8f3fe27d3f06d0
idan <idan441@gmail.com>
2020-04-09 19:28:34 +0300

    styled the HTML, changed the printing of the micro-services

--- main.py
+++ main.py
@@ -30,16 +30,18 @@ def add_warehouse_page():
 	form_wh_name = request.form["wh_name"]
 	form_is_active = request.form["is_active"]
 	conn = db_conn.db_conn()
-	wh.wh_add(conn, form_wh_name, form_is_active)
-	return 'add warehouse page! ' + form_wh_name + form_is_active
+	
+	return render_template('index.html', page_title="Warehouses list: " , warning=wh.wh_add(conn, form_wh_name, form_is_active) , content=wh.wh_list(conn) )
 
 @app.route('/warehouse/update/<wh_id>')
 def update_warehouse_page(wh_id):
 	return 'update warehouse page! ' + wh_id
 
 @app.route('/warehouse/delete/<wh_id>')
-def delete_warehouse_page():
-	return 'delete warehouse page! '
+def delete_warehouse_page(wh_id):
+	form_wh_id = wh_id
+	conn = db_conn.db_conn()
+	return render_template('index.html', page_title="Warehouses list: " , warning=wh.wh_delete(conn, form_wh_id) , content=wh.wh_list(conn) )
 
 
 
--- wh.py
+++ wh.py
@@ -10,19 +10,19 @@ def wh_list(conn):
 	if(len(results)==0): 
 		content += "no warehouses have been added yet! " 
 	else: 
-		content = "<table><th>Id</th><th>name</th><th>activity</th>"
+		content = "<table><th>Id</th><th>name</th><th>activity</th><th>actions</td>"
 		for result in results: 
-			content += "<tr><td>%s</td><td>%s</td><td>%s</td></tr>" % ( result[0], result[1], result[2] )
+			content += "<tr><td>%s</td><td>%s</td><td>%s</td><td><a href=\"/warehouse/delete/%s\">delete</a></td></tr>" % ( result[0], result[1], result[2], result[0] )
 		content += "</table>" 
 		content += "count: " + str(len(results))
 	
-		content += '''<form method="post" action="/warehouse/add">
-						<table>
-							<tr><td>Warehouse name: </td><td><input type="text" name="wh_name" /></td></tr>
-							<tr><td> Is it active? </td><td><input type="text" name="is_active" /></td></tr>
-							<tr><td colspan="2"><input type="submit" value="add" /></td></tr>
-						</table>
-					</form>'''
+	content += '''<form method="post" action="/warehouse/add">
+					<table>
+						<tr><td>Warehouse name: </td><td><input type="text" name="wh_name" /></td></tr>
+						<tr><td> Is it active? </td><td><input type="text" name="is_active" /></td></tr>
+						<tr><td colspan="2"><input type="submit" value="add" /></td></tr>
+					</table>
+				</form>'''
 	return content
 
 
@@ -30,3 +30,14 @@ def wh_add(conn, name, is_active):
 	#Adds a new warehouse to the warehuoses's list
 	conn.execute_query("INSERT INTO warehouses (wh_name, is_active) VALUES ('%s','%s')" % (name, is_active))
 	return " warehouses added! "
+
+def wh_update(conn, name, is_active):
+	#Adds a new warehouse to the warehuoses's list
+	conn.execute_query("INSERT INTO warehouses (wh_name, is_active) VALUES ('%s','%s')" % (name, is_active))
+	return " warehouses added! "
+
+def wh_delete(conn, wh_id):
+	#Adds a new warehouse to the warehuoses's list
+	conn.execute_query("DELETE FROM warehouses WHERE wh_id = '%s' " % (wh_id))
+	return " warehouses removed! "
+
